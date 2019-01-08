4982008d04c13037c63178008b6fb2a3961fc4dc
zwan074 <42155686+zwan074@users.noreply.github.com>
2019-01-08 18:53:43 +1300

    Add files via upload

---
+++ Lab5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_17272381_OnlineStore.MvcApplication" Language="C#" %>
---
+++ Lab5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_17272381_OnlineStore.Startup))]
+namespace _17272381_OnlineStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
